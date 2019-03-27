f73e61392c1569813009afaaa21a943a0f4f7de4
lvpinhe <724596946@qq.com>
2019-03-27 15:52:50 +0800

    1.0

---
+++ travel-common/travel-common-service/src/main/java/com/zb/service/RpcHotelService.java
@@ -0,0 +1,9 @@
+package com.zb.service;
+import com.zb.dto.Page;
+import com.zb.pojo.Hotel;
+
+import java.util.Map;
+
+public interface RpcHotelService {
+    public Page<Hotel> getHotelListByMap(Map<String,Object> map)throws Exception;
+}
