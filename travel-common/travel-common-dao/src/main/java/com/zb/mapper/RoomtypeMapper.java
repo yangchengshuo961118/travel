637761b8d2a3348d68e0a496f0453a161ec38767
lvpinhe <724596946@qq.com>
2019-03-28 18:01:31 +0800

    3.28 update lvpinhe

--- travel-common/travel-common-dao/src/main/java/com/zb/mapper/RoomtypeMapper.java
+++ travel-common/travel-common-dao/src/main/java/com/zb/mapper/RoomtypeMapper.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface RoomtypeMapper {
 
-	public Roomtype getRoomtypeById(@Param(value = "id") Long id)throws Exception;
+	public Roomtype getRoomtypeById(@Param(value = "id") Integer id)throws Exception;
 
 	public List<Roomtype>	getRoomtypeListByMap(Map<String,Object> param)throws Exception;
 
