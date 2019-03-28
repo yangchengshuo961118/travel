da155a4315ee8f1516c5464cb65a92a074a46453
lvpinhe <724596946@qq.com>
2019-03-28 11:02:05 +0800

    3.28 update lvpinhe

--- travel-common/travel-common-service/src/main/java/com/zb/service/RpcRoomTypeService.java
+++ travel-common/travel-common-service/src/main/java/com/zb/service/RpcRoomTypeService.java
@@ -4,7 +4,7 @@ import com.zb.pojo.Roomtype;
 
 import java.util.List;
 
-public interface RpcRoomType {
+public interface RpcRoomTypeService {
     public List<Roomtype> getRoomTypeList(Integer hotelId)throws Exception;
     public Integer updateRoomNum(Integer id,Integer roomNum);
 }
