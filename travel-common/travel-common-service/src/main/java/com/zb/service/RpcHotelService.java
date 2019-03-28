ef6d102659c3eeb172f1a640b723bdc258cf2ea0
lvpinhe <724596946@qq.com>
2019-03-28 16:28:27 +0800

    3.28 update lvpinhe

--- travel-common/travel-common-service/src/main/java/com/zb/service/RpcHotelService.java
+++ travel-common/travel-common-service/src/main/java/com/zb/service/RpcHotelService.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public interface RpcHotelService {
     public Page<Hotel> getHotelListByMap(Integer beginPos,Integer pageSize)throws Exception;
 
-    public Integer insertHotelMap(Map<String,Object> map) throws Exception;
+    public Integer insertHotelMap(Hotel hotel) throws Exception;
 
-    public Integer updateHotelMap(Map<String,Object> map)throws Exception;
+    public Integer updateHotelMap(Hotel hotel)throws Exception;
 }
