a6764829963d5141afef3309da2825c1b723a09b
lvpinhe <724596946@qq.com>
2019-03-29 10:29:23 +0800

    3.29 update lvpinhe

--- travel-common/travel-common-service/src/main/java/com/zb/service/RpcImagesService.java
+++ travel-common/travel-common-service/src/main/java/com/zb/service/RpcImagesService.java
@@ -1,4 +1,13 @@
 package com.zb.service;
 
+import com.zb.pojo.Images;
+
+import java.util.List;
+
 public interface RpcImagesService {
+    public List<Images> getImagesByImagesId(Integer imagesId)throws Exception;
+
+    public Integer insertImagesByImagesId(Images images)throws Exception;
+
+
 }
