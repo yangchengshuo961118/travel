410899de7b25457b472ee0ec7d28bc52ded223ad
lvpinhe <724596946@qq.com>
2019-03-30 13:00:28 +0800

    3.29 update lvpinhe

--- travel-common/travel-common-dto/src/main/java/com/zb/dto/Page.java
+++ travel-common/travel-common-dto/src/main/java/com/zb/dto/Page.java
@@ -27,57 +27,22 @@ public class Page<T> implements Serializable {
      * 页面的总数  .
      */
     private Integer pageCount;
-    /**
-     * 结果集中数据的起始位置  .
-     */
-    private Integer beginPos;
+
     /**
      * List 集合.
      */
     private List<T> rows;
 
-    public Page() {
 
-    }
-    /**
-     * 当前页面 .
-     * 页面的大小 .
-     * @param curpage .
-     * @param pagesize .
-     */
-    public Page(int curpage, Integer pagesize) {
-        this.curPage = curpage;
-        this.pageSize = pagesize;
-    }
-    /**
-     * @param curpage .
-     * @param total .
-     * @param pagesize .
-     */
-    public Page(int curpage, Integer pagesize, Integer total) {
-        super();
-        this.curPage = curpage;//当前页码
-        this.total = total;//总记录数
-        this.pageSize = pagesize;//页码容量
-        //总页数=总记录数total/pageSize（+1）
-        this.pageCount = (total + this.pageSize - 1) /this.pageSize;
-        //下标起始位置：(curPage-1)*pageSize
-        this.beginPos = (curPage-1)*pageSize;
-    }
     /**
      * 总页面数 .
      * @return Integer .
      */
     public Integer getPageCount() {
+        this.pageCount = this.total % this.pageSize == 0 ? this.total / this.pageSize : this.total / this.pageSize + 1;
         return pageCount;
     }
-    /**
-     *  得到页面的当前位置 .
-     * @return Integer .
-     */
-    public Integer getBeginPos() {
-        return beginPos;
-    }
+
     /**
      * @param curPage
      *            the curPage to set
@@ -109,13 +74,7 @@ public class Page<T> implements Serializable {
         this.total = rowCount;
     }
 
-    /**
-     * @param beginPos
-     *            the beginPos to set
-     */
-    public void setBeginPos(Integer beginPos) {
-        this.beginPos = beginPos;
-    }
+
     public Integer getTotal() {
         return total;
     }
